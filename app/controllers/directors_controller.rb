25362a45f6dca200f3ac7ab1c658ee239076b3c6
AvaHenriksen <ava.henriksen@gmail.com>
2017-02-04 20:03:36 -0600

    Director part complete

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -5,9 +5,52 @@ class DirectorsController < ApplicationController
     render("index.html.erb")
   end
 
-  def show
+  def new_form
+    render("new_form.html.erb")
+  end
 
+  def create_row
+    d = Director.new
+    d.name = params[:name]
+    d.bio = params[:bio]
+    d.dob = params[:dob]
+    d.image_url = params[:image_url]
+
+    d.save
+    render("create_row.html.erb")
+  end
+
+  def edit_form
+    d = Director.find(params[:id])
+    d.name = params[:name]
+    d.bio = params[:bio]
+    d.dob = params[:dob]
+    d.image_url = params[:image_url]
+
+    render("edit_form.html.erb")
+  end
+
+  def show
     render("show.html.erb")
   end
+
+  def destroy
+    d = Director.find(params[:id])
+
+    d.destroy
+
+    redirect_to("http://localhost:3000/directors")
+  end
+
+  def update_row
+    d = Director.find(params[:id])
+    d.name = params[:name]
+    d.bio = params[:bio]
+    d.dob = params[:dob]
+    d.image_url = params[:image_url]
+
+    d.save
+    redirect_to("http://localhost:3000/directors/#{params[:id]}")
+end
 
 end
